567212c9dfeaa1b83685c37c4b3a3f35a46ab916
Phuoc1810 <phuoc1122181@gmail.com>
2025-09-12 18:52:19 +0700

    aa
    aa

---
+++ Assets/checkpassword.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class checkpassword : MonoBehaviour
+{
+    public int[] passsword = { 1, 2, 3, 4, 5, 6 };
+    public int[] checkdoor = { 5, 1, 2, 2, 3, 4 };
+    public Door unlock;
+    private void Update()
+    {
+       if (check())
+        {
+            unlock.locks = false;
+        }
+    }
+    public bool check()
+    {
+        for (int i = 0; i < passsword.Length; i++)
+        {
+            //Debug.Log(("point : ", passsword[0], passsword[1], passsword[2], passsword[3], passsword[4], passsword[5]));
+            //Debug.Log(("point : ", checkdoor[0], checkdoor[1], checkdoor[2], checkdoor[3], checkdoor[4], checkdoor[5]));
+            if (passsword[i] != checkdoor[i]) 
+            {
+               Debug.Log( (passsword[i], "", checkdoor[i]));
+                Debug.Log("false");
+                return false;
+
+            }
+        }
+        Debug.Log("true");
+        return true;
+    }
+}
