cb336e11aeb26d398eda4f642b4c14a0c847ab38
Lusmori <thanhtungnguyennnn@gmail.com>
2025-09-14 21:03:32 +0700

    Move file And Update Movement for player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,20 +2,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement Setting")]
     public float moveSpeed = 5f;
     public float runSpeed = 8f;
     public float mouseSensitivity = 100f;
     public Animator animator;
 
+    [Header("Grative setting")]
     private float _velocityVertical = 0f;
     private float _gravity = -9.81f;
 
-    [Header("Head Bobbing Settings")]
     public Transform cameraTransform;
-    //public float walkBobFrequency = 4f;
-    //public float walkBobAmplitude = 0.1f;
-    //public float runBobFrequency = 7f;
-    // public float runBobAmplitude = 0.2f;
 
     private CharacterController controller;
     private float xRotation = 0f;
@@ -36,7 +33,6 @@ public class PlayerMovement : MonoBehaviour
     {
         HandleMovement();
         HandleMouseLook();
-        //HandleHeadBob();
     }
 
     void HandleMouseLook()
@@ -74,37 +70,5 @@ public class PlayerMovement : MonoBehaviour
 
         bool isMoving = moveHorizontal.sqrMagnitude > 0f;
         animator.SetBool("isWalking", isMoving);
-
     }
-
-    //void HandleHeadBob()
-    //{
-    //    Vector3 velocity = new Vector3(controller.velocity.x, 0, controller.velocity.z);
-    //    if (velocity.magnitude > 0.1f) // Moving
-    //    {
-    //        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-    //        float frequency = isRunning ? runBobFrequency : walkBobFrequency;
-    //        float amplitude = isRunning ? runBobAmplitude : walkBobAmplitude;
-
-    //        bobTimer += Time.deltaTime * frequency;
-
-    //        // Up and down bob
-    //        float bobOffsetY = Mathf.Sin(bobTimer) * amplitude;
-
-    //        // Left and right sway (phase-shifted so it feels natural)
-    //        float swayAmplitude = amplitude * 3f; // Smaller than Y bob
-    //        float bobOffsetX = Mathf.Cos(bobTimer * 0.5f) * swayAmplitude;
-
-    //        cameraTransform.localPosition = cameraInitialPosition + new Vector3(bobOffsetX, bobOffsetY, 0);
-    //    }
-    //    else // Idle
-    //    {
-    //        bobTimer = 0;
-    //        cameraTransform.localPosition = Vector3.Lerp(
-    //            cameraTransform.localPosition,
-    //            cameraInitialPosition,
-    //            Time.deltaTime * 5f
-    //        );
-    //    }
-    //}
 }
