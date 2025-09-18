d189d18898471815954f292140d7c425725a6b06
Lusmori <thanhtungnguyennnn@gmail.com>
2025-09-18 22:29:49 +0700

    Update Door

--- Assets/Scripts/ContactDoor/DoorManager.cs
+++ Assets/Scripts/ContactDoor/DoorManager.cs
@@ -4,170 +4,26 @@ using UnityEngine;
 
 public class DoorManager : MonoBehaviour
 {
-    [Header("Door Setting")]
-    [SerializeField] private bool isOpen = false;
-    [SerializeField] private float smooth = 2f;
-    [SerializeField] private float doorOpenAngle = 90f;
-    [SerializeField] private bool isLooked = true;
-    [SerializeField] private bool isBreak = true;
-
-    [Header("Audio Setting")]
-    [SerializeField] private AudioSource audioSource;
-    [SerializeField] private AudioClip fixDoorSound;
-    [SerializeField] private AudioClip doorOpenSound;
-    [SerializeField] private AudioClip lockedSound;
-    [SerializeField] private AudioClip closedSound;
-
-    private Vector3 defaultRotation;
-    private Vector3 openRotation;
-    private bool playerInRange = false;
-    [SerializeField] private TextMeshProUGUI text;
+    private InteractableController controller;
 
     void Start()
     {
-        if (isBreak == true)
-        {
-            isLooked = true;
-            isOpen = false;
-        }
-        defaultRotation = transform.eulerAngles;
-        openRotation = new Vector3(defaultRotation.x, defaultRotation.y + doorOpenAngle, defaultRotation.z);
-        text.text = "";
+        controller = FindObjectOfType<InteractableController>();
     }
-    void Update()
-    {
-        UpdateDoorRotation();
-        HandleInput();
-        UpdateText();
-    }
-    private void HandleInput()
-    {
-        if (Input.GetMouseButtonDown(0) && playerInRange && !isBreak)
-        {
-            TryOpenDoor();
-        }
 
-        else if (Input.GetMouseButtonDown(0) && playerInRange && isBreak)
-        {
-            TryFIxDoor();
-        }
-    }
-    private void TryOpenDoor()
-    {
-        bool hasScrewdriver = InventoryManager.instance.HasItem(Interactable.InteracType.Screwdriver);
-        bool hasKeyMaintance = InventoryManager.instance.HasItem(Interactable.InteracType.KeyMaintance);
-        
-         if (hasScrewdriver)
-        { 
-            if (hasKeyMaintance)
-            {
-                ToggleDoor();
-            }
-            else
-            {
-                PlaySound(lockedSound);
-            }
-        }
-        else
-        {
-            PlaySound(lockedSound);
-            Debug.Log("Dont has a screwdriver, get it!");
-        }
-    }
-    private void TryFIxDoor()
-    {
-        bool hasScrewdriver = InventoryManager.instance.HasItem(Interactable.InteracType.Screwdriver);
-        
-        if (hasScrewdriver )
-        {
-            StartCoroutine(FixDoorProcess());
-        }
-        else
-        {
-            PlaySound(lockedSound);
-        }
-    }
-    private void ToggleDoor()
-    {
-        isLooked = false;
-        isOpen = !isOpen;
-        if (isOpen == false)
-        {
-            PlaySound(closedSound);
-        }
-        else
-        {
-            PlaySound(doorOpenSound);
-        }
-    }
-    private IEnumerator FixDoorProcess()
-    {
-        text.text = "Đang sửa";
-        yield return new WaitForSeconds(2f);
-
-        FixDoor();
-        text.text = "Xong!";
-
-        yield return new WaitForSeconds(1f);
-
-        UpdateText();
-    }
-    private void FixDoor()
-    {
-        isBreak = false;
-        PlaySound(fixDoorSound);
-    }
-    private void UpdateText()
-    {
-        if (!playerInRange)
-        {
-            text.text = "";
-            return;
-        }
-        bool hasScrewdriver = InventoryManager.instance.HasItem(Interactable.InteracType.Screwdriver);
-        bool hasKeyMaintance = InventoryManager.instance.HasItem(Interactable.InteracType.KeyMaintance);
-
-        if (isBreak)
-        {
-            if (!hasScrewdriver)
-            {
-                text.text = "Cửa bị hỏng";
-            }
-        }
-        else
-        {
-            if (!hasKeyMaintance)
-            {
-                text.text = "Đã bị khóa";
-            }
-        }
-
-    }
-    private void UpdateDoorRotation()
-    {
-        Vector3 targetRotation = isOpen ? openRotation : defaultRotation;
-        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetRotation, Time.deltaTime * smooth);
-    }
-    private void PlaySound(AudioClip clip)
-    {
-        if (audioSource != null && clip != null)
-        {
-            audioSource.PlayOneShot(clip);
-        }
-    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && controller != null)
         {
-            playerInRange = true;
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = false;
+            controller.SetDoorPlayerInRange(true);
         }
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && controller != null)
+        {
+            controller.SetDoorPlayerInRange(false);
+        }
+    }
 }
--- Assets/Scripts/ItemManager/InteractableController.cs
+++ Assets/Scripts/ItemManager/InteractableController.cs
@@ -1,4 +1,6 @@
-﻿using Unity.VisualScripting;
+﻿using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InteractableController : MonoBehaviour
@@ -22,6 +24,7 @@ public class InteractableController : MonoBehaviour
     {
         UpdateNormalState();
         HandleInput();
+        UpdateDoorSystem();
     }
     #region Thiết lập ban đầu
     /// <summary>
@@ -86,9 +89,16 @@ public class InteractableController : MonoBehaviour
     #endregion
 
     #region ElectricBox System
+    /// <summary>
+    /// Các biến liên quan
+    /// </summary>
     [Header("Electric Box Setting")]
     [SerializeField] private GameObject fuseObject;
     [SerializeField] private GameObject[] light;
+
+    /// <summary>
+    /// Cập nhật các trạng thái của Electric Box
+    /// </summary>
     private void HandleElectricBoxInteraction()
     {
         if (currentInteractable.Type == Interactable.InteracType.HanldeElectricBox)
@@ -155,13 +165,208 @@ public class InteractableController : MonoBehaviour
         }
     }
     #endregion
+
+    #region
+    ///<summary>
+    ///
+    ///</summary>
+    [Header("Door Setting")]
+    [SerializeField] private bool isOpen = false;
+    [SerializeField] private bool isLocked = true;
+    [SerializeField] private bool isBreak = true;
+    [SerializeField] private float smooth = 2f;
+    [SerializeField] private float doorOpenAngle = 90f;
+
+    [Header("Audio Setting")]
+    [SerializeField] private AudioSource doorAudioSource;
+    [SerializeField] private AudioClip doorOpenSound;
+    [SerializeField] private AudioClip doorClosedSound;
+    [SerializeField] private AudioClip doorLockedSound;
+    [SerializeField] private AudioClip fixDoorSound;
+
+    private Vector3 defaultRotation;
+    private Vector3 openRotation;
+    private bool playerInDoorRange = false;
+    private bool doorInitialized = false;
+
+    [SerializeField] private TextMeshProUGUI doorText;
+    private void InitializeDoorSystem()
+    {
+        if(!doorInitialized &&currentInteractable !=null && currentInteractable.Type == Interactable.InteracType.DoorMaintance)
+        {
+            if (isBreak)
+            {
+                isLocked = true;
+                isOpen = false;
+            }
+
+            defaultRotation = currentInteractable.transform.eulerAngles;
+            openRotation = new Vector3(defaultRotation.x, defaultRotation.y + doorOpenAngle, defaultRotation.z);
+
+            if (doorText!= null)
+            {
+                doorText.text = "";
+            }
+
+            doorInitialized = true;
+        }
+    }
+
+    private void UpdateDoorSystem()
+    {
+        if (currentInteractable != null && currentInteractable.Type == Interactable.InteracType.DoorMaintance)
+        {
+            InitializeDoorSystem();
+            UpdateDoorRotation();
+            UpdateDoorText();
+        }
+    }
+    private void HandleDoorMaintanceInteraction()
+    {
+        if (!playerInDoorRange) return;
+
+        if (!isBreak)
+        {
+            TryOpenDoor();
+        }
+        else
+        {
+            TryFixDoor();
+        }
+    }
+
+    private void TryOpenDoor()
+    {
+        bool hasScrewdriver = InventoryManager.instance.HasItem(Interactable.InteracType.Screwdriver);
+        bool hasKeyMaintance = InventoryManager.instance.HasItem(Interactable.InteracType.KeyMaintance);
+
+        if (hasScrewdriver)
+        {
+            if (hasKeyMaintance)
+            {
+                ToggleDoor();
+            }
+            else
+            {
+                PlayDoorSound(doorLockedSound);
+            }
+        }
+        else
+        {
+            PlayDoorSound(doorLockedSound);
+        }
+    }
+    private void TryFixDoor()
+    {
+        bool hasScrewdriver = InventoryManager.instance.HasItem(Interactable.InteracType.Screwdriver);
+
+        if (hasScrewdriver)
+        {
+            StartCoroutine(FixDoorProcess());
+        }
+        else
+        {
+            PlayDoorSound(doorLockedSound);
+        }
+    }
+    private void ToggleDoor()
+    {
+        isLocked = false;
+        isOpen = !isOpen;
+
+        if (isOpen)
+        {
+            PlayDoorSound(doorOpenSound);
+        }
+        else
+        {
+            PlayDoorSound(doorClosedSound);
+        }
+    }
+    private IEnumerator FixDoorProcess()
+    {
+        if (doorText != null)
+        {
+            doorText.text = "Đang sửa";
+        }
+
+        yield return new WaitForSeconds(2f);
+
+        FixDoor();
+
+        if (doorText != null)
+        {
+            doorText.text = "Xong!";
+        }
+
+        yield return new WaitForSeconds(1f);
+        UpdateDoorText();
+    }
+    private void FixDoor()
+    {
+        isBreak = false;
+        PlayDoorSound(fixDoorSound);
+    }
+    private void UpdateDoorText()
+    {
+        if (doorText == null || !playerInDoorRange)
+        {
+            if (doorText != null)
+            {
+                doorText.text = "";
+                return;
+            }
+        }
+
+        bool hasScrewdriver = InventoryManager.instance.HasItem(Interactable.InteracType.Screwdriver);
+        bool hasKeyMaintance = InventoryManager.instance.HasItem(Interactable.InteracType.KeyMaintance);
+
+        if (isBreak)
+        {
+            if (!hasScrewdriver)
+            {
+                doorText.text = "Cửa bị hỏng";
+            }
+        }
+        else
+        {
+            if (!hasKeyMaintance)
+            {
+                doorText.text = "Đã bị khóa";
+            }
+            else
+            {
+                doorText.text = "";
+            }
+        }
+    }
+    private void UpdateDoorRotation()
+    {
+        if (currentInteractable != null)
+        {
+            Vector3 targetRotation = isOpen ? openRotation : defaultRotation;
+            currentInteractable.transform.eulerAngles = Vector3.Lerp(currentInteractable.transform.eulerAngles, targetRotation, Time.deltaTime * smooth);
+        }
+    }
+    private void PlayDoorSound(AudioClip clip)
+    {
+        if (doorAudioSource != null && clip != null)
+        {
+            doorAudioSource.PlayOneShot(clip);
+        }
+    }
+    public void SetDoorPlayerInRange(bool inRange)
+    {
+        playerInDoorRange = inRange;
+    }
+    #endregion
     #region Pickup item
     ///<summary>
     ///Xu ly input tu nguoi choi
     ///</summary>
     private void HandleInput()
     {
-        if(Input.GetMouseButtonDown(0)&& currentInteractable != null)
+        if(Input.GetMouseButtonDown(0) && currentInteractable != null)
         {
             HandleInteraction();
         }
@@ -188,6 +393,9 @@ public class InteractableController : MonoBehaviour
                 break;
             case Interactable.InteracType.HanldeElectricBox:
                 HandleElectricBoxInteraction();
+                break;
+            case Interactable.InteracType.DoorMaintance:
+                HandleDoorMaintanceInteraction();
                 break;
         }
     }
