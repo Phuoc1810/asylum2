caca8d34fb9b79baa682a335f44b243f2a439c31
krislam2001 <lamtriduc0511@gmail.com>
2025-09-20 14:03:11 +0700

    Cau do 4 - Tao scene test + 4 mieng + scripts

---
+++ Assets/Scripts/Duc scripts/PaperInventory.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PaperInventory : MonoBehaviour
+{
+    public static PaperInventory instance;
+
+    private bool[] collectedPieces = new bool[4];
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    public void AddPiece(int id)
+    {
+        collectedPieces[id] = true;
+
+        if (HasAllPieces())
+        {
+            Debug.Log("All pieces collected!");
+            // Trigger UI assembly
+            Puzzle_note.instance.ShowAssembledNote();
+        }
+    }
+
+    public bool HasAllPieces()
+    {
+        foreach (bool piece in collectedPieces)
+        {
+            if (!piece) return false;
+        }
+        return true;
+    }
+}
---
+++ Assets/Scripts/Duc scripts/PaperPieces.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PaperPieces : MonoBehaviour
+{
+    public int pieceID; // 0 to 3, so you know which part this is
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PaperInventory.instance.AddPiece(pieceID);
+            Destroy(gameObject); // remove piece from world
+        }
+    }
+}
