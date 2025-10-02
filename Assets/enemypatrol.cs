52035d2d11c36eef517c6f362fbdca24030e31af
Phuoc1810 <phuoc1122181@gmail.com>
2025-10-03 00:06:14 +0700

    aa
    aa

---
+++ Assets/enemypatrol.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class enemypatrol : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public int targetPoint;
+    public float speed;
+    public float scale = 0.3f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        targetPoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == patrolPoints[targetPoint].position)
+        {
+            increaseTargetint();
+        }
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position,speed *Time.deltaTime);
+    }
+    void increaseTargetint()
+    {
+        scale *= -1;
+        transform.localScale =new Vector3(0.3f,0.3f,scale);
+        targetPoint++;
+        if (targetPoint >= patrolPoints.Length) { targetPoint = 0; }
+    }
+}
