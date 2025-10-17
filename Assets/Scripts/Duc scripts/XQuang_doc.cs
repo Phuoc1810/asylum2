be04e7f2fe50d1dd3cd91275bb947707083ba6b8
krislam2001 <lamtriduc0511@gmail.com>
2025-10-17 21:31:47 +0700

    Update ghep cau do 5 doctor profile

--- Assets/Scripts/Duc scripts/XQuang_doc.cs
+++ Assets/Scripts/Duc scripts/XQuang_doc.cs
@@ -5,26 +5,19 @@ public class XQuang_doc : MonoBehaviour
     public GameObject DocTextUI;
     public GameObject DocUI;
     bool isOpen;
+    bool isInteracted;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         DocTextUI.SetActive(false);
         DocUI.SetActive(false);
         isOpen = false;
+        isInteracted = false;
     }
 
-    private void OnTriggerEnter(Collider other)
+    void Update()
     {
-        if (other.tag == "doc")
-        {
-            DocTextUI.SetActive(true);
-            Debug.Log("Open doc");
-        }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "doc" && Input.GetKeyDown(KeyCode.E))
+        if (isInteracted && Input.GetKeyDown(KeyCode.P))
         {
             if (!isOpen)
             {
@@ -41,10 +34,40 @@ public class XQuang_doc : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "doc")
+        {
+            DocTextUI.SetActive(true);
+            isInteracted = true;
+            Debug.Log("Open doc");
+        }
+    }
+
+    /*private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "doc" && Input.GetKeyDown(KeyCode.E))
+        {
+            if (!isOpen)
+            {
+                DocUI.SetActive(true);
+                DocTextUI.SetActive(false);
+                isOpen = true;
+            }
+            else
+            {
+                DocUI.SetActive(false);
+                DocTextUI.SetActive(true);
+                isOpen = false;
+            }
+        }
+    }*/
+
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "doc")
         {
+            isInteracted = false;
             DocTextUI.SetActive(false);
         }
     }
